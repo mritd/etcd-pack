43a4defe8e4c88523e1aeb9bc4e37354bf510f02
mritd <mritd@linux.com>
2020-01-11 22:47:59 +0800

    feat(config): update config, add cfssl install script
    update config, add cfssl install script
    
    Signed-off-by: mritd <mritd@linux.com>

--- etc/etcd/cfssl/create.sh
+++ etc/etcd/cfssl/create.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+set -e
 
 cfssl gencert --initca=true etcd-root-ca-csr.json | cfssljson --bare etcd-root-ca
 cfssl gencert --ca etcd-root-ca.pem --ca-key etcd-root-ca-key.pem --config etcd-gencert.json etcd-csr.json | cfssljson --bare etcd
---
+++ etc/etcd/cfssl/install_cfssl.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
+wget https://oss.link/files/cfssl.tar.gz
+tar -zxvf cfssl.tar.gz -C /usr/bin
+rm -f cfssl.tar.gz
