c0c90b0767000868301f13106f7f2733f9c1957b
mritd <mritd@linux.com>
2020-01-11 23:16:15 +0800

    chore(make): add makefile
    add makefile
    
    Signed-off-by: mritd <mritd@linux.com>

---
+++ Makefile
@@ -0,0 +1,15 @@
+BUILD_VERSION   := $(shell cat version)
+
+all:
+	docker run --rm -it -v `pwd`:/etcd-deb mritd/fpm bash -c "cd /etcd-deb && bash ./build.sh"
+
+release: all
+	ghr -u mritd -t ${GITHUB_RELEASE_TOKEN} -replace -recreate -release --debug v${BUILD_VERSION} *.deb
+
+pre-release: all
+	ghr -u mritd -t ${GITHUB_RELEASE_TOKEN} -replace -recreate -prerelease --debug v${BUILD_VERSION} *.deb
+
+clean:
+	rm -rf *.deb *.tar.gz
+
+.PHONY : all release pre-release clean
--- build.sh
+++ build.sh
@@ -2,27 +2,27 @@
 
 set -e
 
-ETCD_VERSION=${1}
+VERSION=$(cat version)
 
-if [ -z "${ETCD_VERSION}" ]; then
+if [ -z "${VERSION}" ]; then
     echo "WARN: etcd version not specified, use default version 3.3.18."
-    ETCD_VERSION="3.3.18"
+    VERSION="3.3.18"
 fi
 
-if [ ! -f "etcd-v${ETCD_VERSION}-linux-amd64.tar.gz" ]; then
+if [ ! -f "etcd-v${VERSION}-linux-amd64.tar.gz" ]; then
     echo "INFO: downloading etcd precompiled binary."
-    wget https://github.com/coreos/etcd/releases/download/v${ETCD_VERSION}/etcd-v${ETCD_VERSION}-linux-amd64.tar.gz
+    wget https://github.com/coreos/etcd/releases/download/v${VERSION}/etcd-v${VERSION}-linux-amd64.tar.gz
 fi
 
 echo "INFO: extract the files."
-tar -zxf etcd-v${ETCD_VERSION}-linux-amd64.tar.gz
-cp etcd-v${ETCD_VERSION}-linux-amd64/etcd* usr/bin
-rm -rf etcd-v${ETCD_VERSION}-linux-amd64
+tar -zxf etcd-v${VERSION}-linux-amd64.tar.gz
+cp etcd-v${VERSION}-linux-amd64/etcd* usr/bin
+rm -rf etcd-v${VERSION}-linux-amd64
 
 echo "INFO: building deb package."
 rm -f *.deb
 fpm -s dir -t deb -n etcd \
-    -v ${ETCD_VERSION} \
+    -v ${VERSION} \
     --vendor "mritd <mritd@linux.com>" \
     --maintainer "mritd <mritd@linux.com>" \
     --license "Apache License 2.0" \
